f0da54542792e1198d9d30bd2a712c2605ea3bc6
NickoValentino <nicko.valentinoo@gmail.com>
2020-07-10 15:57:41 +0800

    :(

--- server/app.js
+++ server/app.js
@@ -6,12 +6,15 @@ const app = express()
 const port = 3000
 const cors = require('cors')
 
+
+app.use(cors())
+
 app.use(session({
   secret: process.env.SESSION,
   resave: false,
   saveUninitialized: false
 }))
-app.use(cors())
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
